503b416f75a84b5aa6121559066c36dc24dadcdb
luisdalmasso <luisdalmasso@gmail.com>
2020-08-24 15:55:11 -0300

    Update chatting.js

--- components/chatting.js
+++ components/chatting.js
@@ -1,8 +1,21 @@
 const router = require('express').Router();
-const { MessageMedia, Location } = require("whatsapp-web.js");
+const { MessageMedia, Location, GroupChat } = require("whatsapp-web.js");
 const request = require('request')
 const vuri = require('valid-url');
 const fs = require('fs');
+
+const sql = require('mssql');
+var configsql = {
+    server: 'desarrollo2012',
+    database: 'ssalud',
+    user: 'websis',
+    password: '123456',
+    port: 1433,
+    options: {
+        encrypt: false ,
+        enableArithAbort: true
+    }
+};
 
 const mediadownloader = (url, path, callback) => {
     request.head(url, (err, res, body) => {
@@ -104,4 +117,114 @@ router.get('/getchats', async (req,res) => {
     })
 });
 
+router.get('/getmessages/:last', async (req,res) => {
+
+    let last = req.params.last;
+    last==undefined ? 100 : last; 
+    if (global.authed =false)
+    {        
+        res.send({status:"error",message:"No Autenticado"});
+    }else {
+        ccmd = "SET QUOTED_IDENTIFIER OFF  select top "+ last +"id, id2, mensaje, origen as autor, ACK as status, tipo, fecha, chat, iif(media>'','true','false') as TieneAdjunto from wa_mensage where dispo_numero = '"+ dispositivo  +"' order by id desc ";
+   // console.log(ccmd)
+    sql.connect(configsql, function (err) {
+        var request = new sql.Request();
+        request.query(ccmd, function (err, recordset) {
+            if (err) {
+                //console.log(err);
+                res.send({status:"error",message:"getmessageserror"});
+            }
+           // console.log(recordset)    
+            res.send({ status:"success", message: recordset});
+        });     
+
+    });
+    }
+});
+router.get('/getmessagesbyid/:chatid', async (req,res) => {
+    let chatid = req.params.chatid;
+    if(chatid==undefined){
+        res.send({status:"error",message:"debe ingresar chatid"});
+    }else{     
+        ccmd = "SET QUOTED_IDENTIFIER OFF  select id, id2, mensaje, origen as autor, ACK as status, tipo, fecha, chat, iif(media>'','true','false') as TieneAdjunto from wa_mensage where chat='"+chatid.toString()+"' and dispo_numero = '"+ dispositivo  +"'";
+       // console.log(ccmd)
+        sql.connect(configsql, function (err) {
+            var request = new sql.Request();
+            request.query(ccmd, function (err, recordset) {
+                if (err) {
+                    //console.log(err);
+                    res.send({status:"error",message:"getmessageserror"});
+                }
+                //console.log(recordset)    
+                res.send({ status:"success", message: recordset});
+            });     
+
+        });
+    };
+});
+
+router.get('/getadjunto/:messageid', async (req,res) => {
+    let messageid = req.params.messageid;
+    if(messageid==undefined){
+        res.send({status:"error",message:"debe ingresar messageid"});
+    }else{     
+        ccmd = "SET QUOTED_IDENTIFIER OFF  select id, media, mime from wa_mensage where id2='"+messageid.toString()+"' and dispo_numero = '"+ dispositivo  +"'";
+        sql.connect(configsql, function (err) {
+            var request = new sql.Request();
+            request.query(ccmd, function (err, recordset) {
+                if (err) {
+                    console.log(err);
+                    res.send({status:"error",message:"getmessageserror"});
+                }
+                //console.log(recordset) ;
+                if (recordset.recordset[0].media = undefined )  {
+                    res.send({status:"error",message:"No se encuentra Adjunto para el mensaje"});
+            }
+                var media = recordset.recordset[0].media ;
+                var mime = recordset.recordset[0].mime ;
+                var id = recordset.recordset[0].id ;
+                var FilePath= id.toString();
+                aext = mime.split('/');
+                sext = aext[1].includes('ogg') ? '.ogg' : '.'+aext[1];
+                FilePath = config.pathmedia+FilePath.replace(/"/gi,'')+sext;
+                fs.writeFileSync(FilePath, media, 'base64');
+                FilePath=__dirname+FilePath.substring(12)                ;
+                res.download(FilePath); // Set disposition and send it.                
+            });     
+
+        });
+    };
+});
+
 module.exports = router;
+
+//client.getChats().then((chats) => { chats.fetchMessages().then((resp) =>
+//    { res.send({status:"success", message: resp }) }
+//).catch(() => {  res.send({status:"error",message:"getchatserror"})});
+//});
+
+// client.getChats().then((chats) => {
+//     for (const n_chat of chats) {
+//         var n_id = n_chat.id;        
+//         n_chat.fetchMessages().then(messages);
+//         res.send({ status:"success", message: messages});
+//     };    
+//     }).catch(() => {   res.send({status:"error",message:"getchatserror"})
+//     });
+// });
+
+
+
+
+// client.getChat().then((chat) => { chat.fetchMessage().then((resp) => { send status here } } 
+//    client.getChats().then((chat) => { chat.fetchMessage().then((resp) => { message: resp 
+// })
+//    .catch(() => {              
+//             res.send({status:"error",message:"getmessageserror"})      
+//         });
+//     });
+// });     
+
+   //client.getChatById(phone+"@c.us").fetchMessages().then((messages) => {
+   //       res.send({ status:"success", message: messages });
+
